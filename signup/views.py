b4ecd9d38e328d05f93231cb2b22efa658f7daef
nidhiu03@gmai.com <nidhiu03@gmai.com>
2022-03-23 13:17:38 +0530

    commit

--- signup/views.py
+++ signup/views.py
@@ -1,33 +1,24 @@
+from urllib import response
 from django.shortcuts import render
-#?import mysql.connector as sql
+import mysql.connector as sql
 fn=''
 ln=''
 s=''
 em=''
 pwd=''
+m=""
 
 # Create your views here.
 def signaction(request):
-    global fn,ln,s,em,pwd
+    global fn,ln,s,em,pwd,m
     if request.method=='POST':
       m=sql.connector(host="localhost:3306",user="root",passwd="Pratik@007",database='website')
-    cursor=m.cursor()
-    d=request.Post
-    for key,value in d.items():
-        if key=="fitst_name":
-            fn==value
-        if key=="last_name":
-            ln==value
-        if key=="sex":
-            s==value
-        if key=="email":
-            em==value
-        if key=="password":
-            pwd==value
+    
+            
     
     c="insert into users values('{}','{}','{}','{}','{}')".format(fn,ln,s,em,pwd)
-    cursor.execute(c)
     m.commit()
 
 
     return render(request,'signup_page.html')
+    return response

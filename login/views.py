16a29d57778a88943048253d52286a171f5ff3db
nidhiu03@gmai.com <nidhiu03@gmai.com>
2022-03-23 13:18:10 +0530

    commit

--- login/views.py
+++ login/views.py
@@ -1,5 +1,22 @@
+from sqlite3 import Cursor
+from urllib import request, response
 from django.shortcuts import render
+import mysql.connector as sql
+from django.shortcuts import redirect, render
+
+em=''
+pwd=''
+m=''
 
 # Create your views here.
 def loginaction(request):
-    return None
+    global em,pwd,m
+    if request.method=="POST":
+        m=sql.connector(host="localhost",user="root",password="Pratik@007",database="web")
+
+        c="select * from users where email '{}' and password '{}'".format(em,pwd)
+         
+    
+
+    
+        return (request,'login_page.html')
--- website/urls.py
+++ website/urls.py
@@ -17,9 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from signup.views import signaction
 from login.views import loginaction
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/',signaction),
-    path('',loginaction),
+    path('login/',loginaction),
 ]
